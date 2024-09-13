7a7c2f1cc0e64f2fe92e4e843673cae05c182f4a
schnekenberg <gschnekenberg@gmail.com>
2024-09-12 22:26:18 -0300

    Create Model.h

---
+++ Model.h
@@ -0,0 +1,18 @@
+/*
+DOM_Model.h
+DOM - Projeto Domino
+2023
+*/
+
+char alfabeto[] = "abcdefghijklmnopqrstuvwxyz";
+
+int jvez; 
+int mesaE; //extremidade esquerda da mesa
+int mesaD; //extremidade direita da mesa
+int jogcomp; //2; o computador e' o jogador 2
+int qtmesa; //quantidade de pedras na mesa
+int passou; //quantidade de vezes que passou consecutivamente
+int win; //variavel que define a vitoria
+int playerAuto; //"player automatico" variavel que indica qual dos jogadores e' o computador em um jogo contra o computador
+int last_L1; //ultima pedra que foi jogada lado 1
+int last_L2; //ultima pedra que foi jogada lado 2
